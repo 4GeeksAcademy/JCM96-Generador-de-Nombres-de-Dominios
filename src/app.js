fdf66277f88b5d0d204040e1b7fe6719131a18dd
Jhon Cordova <jhon.a.cordova.martinez@gmail.com>
2025-06-26 14:47:19 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+ let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+function generateDomainNames(pronouns, adjs, nouns) {
+  let domainNames = [];
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let j = 0; j < adjs.length; j++) {
+      for (let k = 0; k < nouns.length; k++) {
+        let domain = pronouns[i] + adjs[j] + nouns[k] + '.com';
+        domainNames.push(domain);
+      }
+    }
+  }
+  return domainNames;
+}
+
+let generatedDomains = generateDomainNames(pronoun, adj, noun);
+
+// Imprimir los dominios generados
+for (let i = 0; i < generatedDomains.length; i++) {
+  console.log(generatedDomains[i]);
+}
 };
